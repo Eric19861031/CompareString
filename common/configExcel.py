be95c61781d03106b3f3cc36fbced5af82883b6f
y90043 <yaoxiaohan@foxmail.com>
2018-04-27 16:24:15 +0800

    update

--- common/configExcel.py
+++ common/configExcel.py
@@ -14,31 +14,33 @@ class MyExcel:
         self.sheet = self.wb[sheet]
 
     def get_en(self,id):
+        str1 = ''
         maxValue = self.sheet.max_row
         for i in range(1, maxValue, 1):
             if self.sheet.cell(row=i, column=4).value == int(id):
                 if self.sheet.cell(row=i, column=8).value is None:
-                    str = self.sheet.cell(row=i, column=5).value
+                    str1 = self.sheet.cell(row=i, column=5).value
                 else:
-                    str = self.sheet.cell(row=i, column=8).value
-        return str
+                    str1 = self.sheet.cell(row=i, column=8).value
+        return str1
 
     def get_cn(self,id):
+        str2 = ''
         maxValue = self.sheet.max_row
         for i in range(1, maxValue, 1):
             if self.sheet.cell(row=i, column=4).value == int(id):
                 if self.sheet.cell(row=i, column=9).value is None:
-                    str = self.sheet.cell(row=i, column=6).value
+                    str2 = self.sheet.cell(row=i, column=6).value
                 else:
-                    str = self.sheet.cell(row=i, column=9).value
-        return str
+                    str2 = self.sheet.cell(row=i, column=9).value
+        return str2
 
     def closeOp(self):
         self.wb.close()
 
-# if __name__ == '__main__':
-#     ExcelFile = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\Lang_uage.xlsx'
-#     Demo = MyExcel(ExcelFile)
-#     Demo.selectSheet('Param')
-#     print Demo.get_cn('1') + ' and ' + Demo.get_en('1')
-#     Demo.closeOp()
+if __name__ == '__main__':
+    ExcelFile = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\Lang_uage.xlsx'
+    Demo = MyExcel(ExcelFile)
+    Demo.selectSheet('Param')
+    print Demo.get_cn('1') + ' and ' + Demo.get_en('1')
+    Demo.closeOp()
--- progam.py
+++ progam.py
@@ -1,14 +1,42 @@
-from Excel import FindExcel
+# -*- coding: utf-8 -*-
+import sys
+
 from common.configDB import MyDB
+from common.configExcel import MyExcel
 from common.otherFunction import getCompareId_list
 
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+sys.setdefaultencoding('utf-8')
 
 if __name__ == '__main__':
     JsonFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\RCDB\\Json\\Description.txt'
     DataBaseFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\XMLToolCompactData.sdf;'
-    Demo = MyDB(DataBaseFileDir)
-    Demo.connectDB()
+    ExcelFile = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\Lang_uage.xlsx'
+
+    Exceloj = MyExcel(ExcelFile)
+    Exceloj.selectSheet('Param')
+
+    DBoj = MyDB(DataBaseFileDir)
+    DBoj.connectDB()
+
     list = getCompareId_list(JsonFileDir)
+
     for id in list:
         # print Demo.getalias_Cn(id) + ' and ' + Demo.getalias_En(id) + '\n'
-        print FindExcel(int(id),'Lang_uage.xlsx')[0]
+        print 'ID为' + id + '的中英文比对如下\n'
+
+        if Exceloj.get_cn(id) == DBoj.getalias_Cn(id):
+            print '中文比对结果：ID为' + id + ': pass' + '\n'
+        else:
+            print '中文比对结果：ID为' + id + DBoj.getalias_Cn(id) + ' 翻译与需求不符，需求为 ' + Exceloj.get_cn(id) + '\n'
+
+        if Exceloj.get_en(id) == DBoj.getalias_En(id):
+            print '英文比对结果：ID为' + id + ': pass' + '\n'
+        else:
+            print '英文比对结果：ID为' + id + DBoj.getalias_En(id) + ' 翻译与需求不符，需求为 ' + Exceloj.get_en(id) + '\n'
+
+
+    DBoj.closeDB()
+    Exceloj.closeOp()
+
