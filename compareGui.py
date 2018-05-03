66096b6b8379e79498f2d58d52405a545d43fd79
y90043 <yaoxiaohan@foxmail.com>
2018-05-03 10:14:53 +0800

    update

--- compareGui.py
+++ compareGui.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 # Form implementation generated from reading ui file 'compareGui.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
@@ -5,9 +6,15 @@
 # WARNING! All changes made in this file will be lost!
 import os
 import sys
+
+import time
 from PyQt4 import QtCore, QtGui
 
-from progam import programFunction
+from common.configDB import MyDB
+from common.configExcel import MyExcel
+from common.configFile import Myfile
+from common.otherFunction import getCompareId_list
+# from progam import Main_program
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -23,10 +30,14 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+sys.setdefaultencoding('utf-8')
+
 class Ui_MainWindow(QtGui.QMainWindow):
-    JsonPath = ''
-    ExcePath = ''
-    DataPath = ''
+    JsonPath,ExcePath,DataPath = '','',''
+
 
     def __init__(self):
         super(Ui_MainWindow, self).__init__()
@@ -64,8 +75,8 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.program.setGeometry(QtCore.QRect(25, 180, 101, 23))
         self.program.setObjectName(_fromUtf8("program"))
         self.program.clicked[bool].connect(self.confirmprogram)
-        self.programStatusLable = QtGui.QLabel("none", self)
-        self.programStatusLable.move(25, 160)
+        # self.programStatusLable = QtGui.QLabel("none", self)
+        # self.programStatusLable.move(25, 160)
 
         self.openResult = QtGui.QPushButton(self.centralwidget)
         self.openResult.setGeometry(QtCore.QRect(170, 180, 101, 23))
@@ -76,6 +87,9 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
+
+        self.progress = QtGui.QProgressBar(self)
+        self.progress.setGeometry(26,210,130,20)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -107,9 +121,55 @@ class Ui_MainWindow(QtGui.QMainWindow):
         print self.DataPath
 
     def confirmprogram(self):
+        success, fail = 0.0, 0.0
         self.program.setEnabled(False)
-        programFunction(self.JsonPath, self.ExcePath, self.DataPath)
-        self.programStatusLable.setText('completed')
+        # Demo = Main_program(self)
+        # Demo.programFunction(self.JsonPath, self.ExcePath, self.DataPath)
+
+        path = os.path.abspath('Result')
+        name = '//' + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + 'Result'
+        type = 'txt'
+
+        Exceloj = MyExcel(self.ExcePath)
+        Exceloj.selectSheet('Param')
+
+        DBoj = MyDB(self.DataPath)
+        DBoj.connectDB()
+
+        list = getCompareId_list(self.JsonPath)
+        total = len(list) * 2
+        writeStream = Myfile(path, name, type)
+        writeStream.open_W()
+
+        for id in list:
+            writeStream.file.write('ID为  ' + id + '  的中英文比对如下\n')
+            if Exceloj.get_cn(id) == DBoj.getalias_Cn(id):
+                writeStream.file.write('中文比对结果：ID为' + id + ': pass' + '   ----------------------   ')
+                success += 1.0
+            else:
+                writeStream.file.write(
+                    '中文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_Cn(id) + '   翻译与需求不符，需求为：' + Exceloj.get_cn(
+                        id) + '   ----------------------   ')
+                fail += 1.0
+
+            if Exceloj.get_en(id) == DBoj.getalias_En(id):
+                writeStream.file.write('英文比对结果：ID为' + id + ': pass' + '\n')
+                success += 1.0
+            else:
+                writeStream.file.write(
+                    '英文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_En(id) + '   翻译与需求不符，需求为：' + Exceloj.get_en(
+                        id) + '\n')
+                fail += 1.0
+
+            if (success + fail) / total >= 100:
+                self.program.setText('completed')
+            self.progress.setValue((success + fail) * 100 / total)
+
+        writeStream.file.write('结论：总数 ：' + str(total) + ' ； 成功数目 ：' + str(success) + '； 失败数目 ：' + str(
+            fail) + '； 成功率 ：' + str(success / total))
+
+        DBoj.closeDB()
+        Exceloj.closeOp()
         self.program.setEnabled(True)
 
 # def Run_Gui():
--- progam.py
+++ progam.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import time
+# from PyQt4 import QtGui
 
 from common.configDB import MyDB
 from common.configExcel import MyExcel
@@ -13,51 +14,62 @@ if sys.getdefaultencoding() != 'utf-8':
     reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def programFunction(JsonFileDir,ExcelFile,DataBaseFileDir):
+class Main_program():
 
-    # JsonFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\RCDB\\Json\\Description.txt'
-    # DataBaseFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\XMLToolCompactData.sdf;'
-    # ExcelFile = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\Lang_uage.xlsx'
+    total,success,fail = 0.0,0.0,0.0
+    # progress = None
 
-    total, success, fail = 0, 0, 0
+    # def __init__(self,Ui_MainWindow):
+    #     self.progress = QtGui.QProgressBar(Ui_MainWindow)
+    #     self.progress.setGeometry(25, 170, 120, 5)
 
-    # JsonFileDir =
-    # DataBaseFileDir =
-    # ExcelFile =
+    # def progress(self):
+    #     return (self.success+self.fail)*100/self.total
 
-    path = os.path.abspath('Result')
-    name = '//' + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + 'Result'
-    type = 'txt'
+    def programFunction(self,JsonFileDir,ExcelFile,DataBaseFileDir):
 
-    Exceloj = MyExcel(ExcelFile)
-    Exceloj.selectSheet('Param')
+        # JsonFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\RCDB\\Json\\Description.txt'
+        # DataBaseFileDir = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\XMLToolCompactData.sdf;'
+        # ExcelFile = 'C:\\Program Files (x86)\\Hytera\\MDM\\MDMAdminClient\\MDMAdminClient\\CPS\\addins\\G2_CPS_Public_V2.0.08.011.plug_G2CPS_Public\\Lang_uage.xlsx'
 
-    DBoj = MyDB(DataBaseFileDir)
-    DBoj.connectDB()
+        # JsonFileDir =
+        # DataBaseFileDir =
+        # ExcelFile =
 
-    list = getCompareId_list(JsonFileDir)
-    writeStream = Myfile(path,name,type)
-    writeStream.open_W()
+        path = os.path.abspath('Result')
+        name = '//' + time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())) + 'Result'
+        type = 'txt'
 
-    for id in list:
-        writeStream.file.write('ID为  ' + id + '  的中英文比对如下\n')
-        total += 2.0
-        if Exceloj.get_cn(id) == DBoj.getalias_Cn(id):
-            writeStream.file.write( '中文比对结果：ID为' + id + ': pass' + '   ----------------------   ')
-            success += 1.0
-        else:
-            writeStream.file.write( '中文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_Cn(id) + '   翻译与需求不符，需求为：' + Exceloj.get_cn(id) + '   ----------------------   ')
-            fail += 1.0
+        Exceloj = MyExcel(ExcelFile)
+        Exceloj.selectSheet('Param')
 
-        if Exceloj.get_en(id) == DBoj.getalias_En(id):
-            writeStream.file.write( '英文比对结果：ID为' + id + ': pass' + '\n')
-            success += 1.0
-        else:
-            writeStream.file.write( '英文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_En(id) + '   翻译与需求不符，需求为：' + Exceloj.get_en(id) + '\n')
-            fail += 1.0
+        DBoj = MyDB(DataBaseFileDir)
+        DBoj.connectDB()
 
-    writeStream.file.write( '结论：总数 ：' + str(total) + ' ； 成功数目 ：' + str(success) + '； 失败数目 ：' + str(fail) + '； 成功率 ：' + str(success/total))
+        list = getCompareId_list(JsonFileDir)
+        self.total = len(list)*4
+        writeStream = Myfile(path,name,type)
+        writeStream.open_W()
 
-    DBoj.closeDB()
-    Exceloj.closeOp()
+        for id in list:
+            # self.progress.setValue((self.success+self.fail)/self.total)
+            writeStream.file.write('ID为  ' + id + '  的中英文比对如下\n')
+            if Exceloj.get_cn(id) == DBoj.getalias_Cn(id):
+                writeStream.file.write( '中文比对结果：ID为' + id + ': pass' + '   ----------------------   ')
+                self.success += 1.0
+            else:
+                writeStream.file.write( '中文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_Cn(id) + '   翻译与需求不符，需求为：' + Exceloj.get_cn(id) + '   ----------------------   ')
+                self.fail += 1.0
+
+            if Exceloj.get_en(id) == DBoj.getalias_En(id):
+                writeStream.file.write( '英文比对结果：ID为' + id + ': pass' + '\n')
+                self.success += 1.0
+            else:
+                writeStream.file.write( '英文比对结果：ID为' + id + '；界面显示为：' + DBoj.getalias_En(id) + '   翻译与需求不符，需求为：' + Exceloj.get_en(id) + '\n')
+                self.fail += 1.0
+
+        writeStream.file.write( '结论：总数 ：' + str(self.total) + ' ； 成功数目 ：' + str(self.success) + '； 失败数目 ：' + str(self.fail) + '； 成功率 ：' + str(self.success/self.total))
+
+        DBoj.closeDB()
+        Exceloj.closeOp()
 
